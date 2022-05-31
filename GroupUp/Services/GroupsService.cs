b42d6d364f081ebe4d1ec36ce507d2edbf17c68e
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-05-31 15:37:12 -0600

    init & controller

---
+++ GroupUp/Services/GroupsService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using GroupUp.Models;
+
+namespace GroupUp.Services
+{
+  public class GroupsService
+  {
+    internal List<Group> Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Group Get(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Group Create(Group group)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Group Update(Group group)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int id1, string id2)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
